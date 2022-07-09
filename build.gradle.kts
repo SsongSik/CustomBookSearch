80ed42720c45485c52f676b28405b26f3ae7dcc7
sgsk88 <sgsk88@nate.com>
2022-07-10 01:35:10 +0900

    [feat] build KTS 마이그레이션 완료

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "7.1.2" apply false
-    id("com.android.library") version "7.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    id(Plugins.ANDROID_APPLICATION) version Versions.AGP apply false
+    id(Plugins.ANDROID_LIBRARY) version Versions.AGP apply false
+    id(Plugins.KOTLIN_ANDROID) version Versions.KOTLIN apply false
     //Secrets Plugin
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.4.1" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10" apply false
-    id("com.google.dagger.hilt.android") version "2.41" apply false
+    id(Plugins.SECRETS_GRADLE_PLUGIN) version Versions.SECRETS_GRADLE apply false
+    id(Plugins.SAFEARGS) version Versions.NAVIGATION apply false
+    id(Plugins.KOTLIN_SERIALIZATION) version Versions.KOTLIN apply false
+    id(Plugins.DAGGER_HILT) version Versions.HILT apply false
 }
 
 //task clean(type: Delete) {
