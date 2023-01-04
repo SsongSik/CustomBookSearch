5c2d10d01ac1fe7f1345702bf9877a1f9424f99c
sgsk88 <sgsk88@nate.com>
2023-01-04 14:49:03 +0900

    [feat] Roboletric Test 작성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,11 @@ android {
     kapt {
         correctErrorTypes = true
     }
+    testOptions{
+        unitTests{
+            isIncludeAndroidResources = true
+        }
+    }
 }
 
 dependencies {
@@ -55,6 +60,9 @@ dependencies {
     testImplementation(Testing.JUNIT4)
     testImplementation("androidx.test.ext:truth:1.4.0")
     testImplementation("androidx.test:runner:1.4.0")
+    testImplementation("org.robolectric:robolectric:4.8.1")
+    testImplementation("androidx.test.ext:junit:1.1.3")
+    testImplementation("androidx.test:core:1.4.0")
 
     androidTestImplementation(Testing.ANDROID_JUNIT)
     androidTestImplementation(Testing.ESPRESSO_CORE)
--- app/src/test/java/com/example/booksearch/ui/view/MainActivityTest.kt
+++ app/src/test/java/com/example/booksearch/ui/view/MainActivityTest.kt
@@ -1,18 +1,32 @@
 package com.example.booksearch.ui.view
 
-import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
-import org.junit.After
-import org.junit.Assert.*
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
+import androidx.test.runner.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
 
-@RunWith(AndroidJUnit4::class)
+//액티비티를 로컬 유닛테스트로 할 수 있음
+//RobolectricTestRunner 로 인해
+//@RunWith(RobolectricTestRunner::class)
+//class MainActivityTest{
+//
+//    //로버레트릭이 4.0으로 업그레이드되면서 통합됨
+//    //setupActivity 는 사용되지 않고 제거됨.
+//    @Test
+//    @SmallTest
+//    fun test_activity_state() {
+//        val controller = Robolectric.setupActivity(MainActivity::class.java)
+//        val activityState = controller.lifecycle.currentState.name
+//        assertThat(activityState).isEqualTo("RESUMED")
+//    }
+//}
+
+@RunWith(androidx.test.ext.junit.runners.AndroidJUnit4::class)
 class MainActivityTest {
 
 //    private lateinit var activityScenario: ActivityScenario<MainActivity>
